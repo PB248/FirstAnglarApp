b241e39d40c1233ae2786d556ceecc371f095b52
Peter Bennett <peter@ApplecorePixels.com.au>
2014-12-27 23:08:07 +1100

    latest.

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -7,42 +7,85 @@ angular.module('ControlRoom.controllers', [])
         // $scope.students = dsStudents;
      }
 ])
-
-//.controller('MyCtrlMessages', ['$scope', '$http', '$templateCache',
-//    function ($scope, $http, $templateCache) {
-//        $scope.status;
-//        $scope.messageList = [];
-//   
-//        $scope.method = 'GET';
-//        $scope.url = '/ctrlRoom.php?method=message';
-//
-//        $scope.fetch = function() {
-//            $http({method: $scope.method, url: $scope.url, cache: $templateCache}).
-//                success(function(data, status) {
-//                    $scope.messageList = data.data;
-//                    $scope.status = status;
-//                }).
-//                error(function(data, status) {
-//                    $scope.messageList = data || "Request failed";
-//                    $scope.status = status;
-//                });
-//            };
-//    }
-//]);
         
 .controller('MyCtrlMessages', ['$scope', 'dataFactory',
     function ($scope, dataFactory) {
         $scope.status;
         $scope.messageList = [];
+        $scope.yearLevels = [];
    
-        dataFactory.getMessages().
+        $scope.isCreating = false;
+        
+        dataFactory.getMessagesData().
             success(function(data, status) {
                 $scope.status = status;
-                $scope.messageList = data.data;
+                $scope.messageList = data.data[0].data;
+                $scope.yearLevels = data.data[1].yearlevel;
             }).
             error(function(data, status) {
                 $scope.messageList = data || "Request failed";
                 $scope.status = status;
             });
+        
+        function resetCreateForm() {
+            $scope.newMessage = {
+                groupguid: '',
+                title: '',
+                body: '',
+                who: ''
+            };
+        }
+        
+        //--------------------------------------------------------
+        // CRUD
+        //--------------------------------------------------------
+        function createMessage(message) {
+            message.groupguid = '123test';
+            message.who = 'EveryOne';
+            
+            var submitForm = true;
+            
+            if (message.title == '') {
+                $scope.errorTitle = true;
+                submitForm = false;
+            }
+                
+            if (submitForm) {
+                dataFactory.getMessagesNew(message).
+                    success(function(data, status) {
+                        $scope.messageList.unshift(message);
+                    }).
+                    error(function(data, status) {
+                        $scope.messageList = data || "Request failed";
+                        $scope.status = status;
+                    });
+                resetCreateForm();
+                $scope.isCreating = false;
+            }
+        }
+        
+        function updateMessage(message) {
+            var index = _.findIndex($scope.messageList, function (b) {
+                return b.id == message.id
+            });
+            $scope.messageList[index] = message;
+        }
+        
+        $scope.createMessage = createMessage;
+        $scope.updateMessage = updateMessage;
+        //--------------------------------------------------------
+        // Creating States
+        //--------------------------------------------------------
+        function startCreating() {
+            $scope.isCreating = true;
+            
+            resetCreateForm();
+        }
+        $scope.startCreating = startCreating;
+        
+        function cancelCreating() {
+            $scope.isCreating = false;
+        }
+        $scope.cancelCreating = cancelCreating;
     }
 ]);
--- scripts/services.js
+++ scripts/services.js
@@ -4,11 +4,16 @@ angular.module('ControlRoom.services', [])
     function ($http) {
         var dataFactory = {};
 
-//        var method = 'GET';
-//        var url = '/ctrlRoom.php?method=';
+        var pUrl = 'http://peter.app4.ws/ctrlRoom.php?method=';
 
-        dataFactory.getMessages =  function() {
-            return $http({method: 'GET', url: '/ctrlRoom.php?method=message'})
+        dataFactory.getMessagesData =  function() {
+            return $http({method: 'GET', url: pUrl + 'messageData'})
+        };
+        dataFactory.getMessagesYearLevel =  function() {
+            return $http({method: 'GET', url: pUrl + 'messageYearLevel'})
+        };
+        dataFactory.getMessagesNew =  function(data) {
+            return $http({method: 'GET', url: pUrl + 'messageNew&mdata='+JSON.stringify(data)})
         };
 
     return dataFactory;
